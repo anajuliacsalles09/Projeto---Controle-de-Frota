a3027de6c97149a04f64c3c85b0db653fd471f67
Lara <larafaustinha12@gmail.com>
2026-08-21 14:01:31 -0300

    Adiciona models de veiculo abastecimento e manutencao

--- frota/admin.py
+++ frota/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Usuario, Viagem
+from .models import Usuario, Viagem, Veiculo, Abastecimento, Manutencao
 
 
 admin.site.register(Usuario)
 admin.site.register(Viagem)
+admin.site.register(Veiculo)
+admin.site.register(Abastecimento)
+admin.site.register(Manutencao)
