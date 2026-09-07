06ef9e6ae24c09ecfa1312682c8a3299319c94eb
Lara Fausto <larafaustinha12@gmail.com>
2026-09-07 11:16:59 -0300

    Implementa CRUD de veículo

---
+++ veiculo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.veiculo_list, name='veiculo_list'),
+    path('<int:id>/', views.veiculo_detail, name='veiculo_detail'),
+    path('<int:id>/editar/', views.veiculo_update, name='veiculo_update'),
+    path('<int:id>/excluir/', views.veiculo_delete, name='veiculo_delete'),
+]
--- veiculo/views.py
+++ veiculo/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Veiculo
+
+
+def veiculo_list(request):
+    objetos = Veiculo.objects.all()
+
+    return render(request, 'veiculo/veiculo_list.html', {
+        'objetos': objetos
+    })
+
+
+def veiculo_detail(request, id):
+    veiculo = Veiculo.objects.get(id=id)
+
+    return render(request, 'veiculo/veiculo_detail.html', {
+        'veiculo': veiculo
+    })
+
+
+def veiculo_update(request, id):
+    veiculo = Veiculo.objects.get(id=id)
+
+    if request.method == 'POST':
+        veiculo.placa = request.POST['placa']
+        veiculo.modelo = request.POST['modelo']
+        veiculo.marca = request.POST['marca']
+        veiculo.ano = request.POST['ano']
+        veiculo.quilometragem = request.POST['quilometragem']
+        veiculo.status = request.POST['status']
+        veiculo.capacidade_carga = request.POST['capacidade_carga']
+
+        veiculo.save()
+
+        return render(request, 'veiculo/veiculo_detail.html', {
+            'veiculo': veiculo
+        })
+
+    return render(request, 'veiculo/veiculo_update.html', {
+        'veiculo': veiculo
+    })
+
+
+def veiculo_delete(request, id):
+    veiculo = Veiculo.objects.get(id=id)
+
+    if request.method == 'POST':
+        veiculo.delete()
+
+        return render(request, 'veiculo/veiculo_list.html', {
+            'objetos': Veiculo.objects.all()
+        })
+
+    return render(request, 'veiculo/veiculo_delete.html', {
+        'veiculo': veiculo
+    })
